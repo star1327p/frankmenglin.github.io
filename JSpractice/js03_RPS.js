9df009af2b71ada25f693c3f5c571a7a6f582141
Your Name <you@example.com>
2020-05-28 11:40:53 -0700

    edit RPS

--- JSpractice/js03_RPS.js
+++ JSpractice/js03_RPS.js
@@ -1,51 +1,54 @@
 var playerscore = 0;
 var computerscore = 0;
 
-scoreupdate.innerHTML = "Hello";
+
 function playerRock(){
+    var gameresult=document.getElementById("game result");
     var ComputerPlay=Math.random();
     if (ComputerPlay<1.0/3.0){
-        alert("You play Rock! I play Scissors! You win!");
+        gameresult.innerHTML = "You play Rock! I play Scissors! You win!";
         playerscore++;
         var scoreupdate = document.getElementById("player score");
         scoreupdate.innerHTML = "Your Score: "+playerscore;
     }else if (ComputerPlay<2.0/3.0){
-        alert("You play Rock! I play Rock! Tie.");
+        gameresult.innerHTML="You play Rock! I play Rock! Tie.";
     }else{
 
-        alert("You play Rock! I play Paper! You lose!");
+        gameresult.innerHTML="You play Rock! I play Paper! You lose!";
         computerscore++;
         var scoreupdate = document.getElementById("computer score");
         scoreupdate.innerHTML = "My Score: "+computerscore;
     }
 }
 function playerPaper(){
+    var gameresult=document.getElementById("game result");
     var ComputerPlay=Math.random();
     if (ComputerPlay<1.0/3.0){
-        alert("You play Paper! I play Scissors! You lose!");
+        gameresult.innerHTML="You play Paper! I play Scissors! You lose!";
         computerscore++;
         var scoreupdate = document.getElementById("computer score");
         scoreupdate.innerHTML = "My Score: "+computerscore;
     }else if (ComputerPlay<2.0/3.0){
-        alert("You play Paper! I play Rock! You win!");
+        gameresult.innerHTML="You play Paper! I play Rock! You win!";
         playerscore++;
         var scoreupdate = document.getElementById("player score");
         scoreupdate.innerHTML = "Your Score: "+playerscore;
     }else{
-        alert("You play Paper! I play Paper! Tie.");
+        gameresult.innerHTML="You play Paper! I play Paper! Tie.";
     }
 }
 function playerScissors(){
+    var gameresult=document.getElementById("game result");
     var ComputerPlay=Math.random();
     if (ComputerPlay<1.0/3.0){
-        alert("You play Scissors! I play Scissors! Tie.");
+        gameresult.innerHTML="You play Scissors! I play Scissors! Tie.";
     }else if (ComputerPlay<2.0/3.0){
-        alert("You play Scissors! I play Rock! You lose!");
+        gameresult.innerHTML="You play Scissors! I play Rock! You lose!";
         computerscore++;
         var scoreupdate = document.getElementById("computer score");
         scoreupdate.innerHTML = "My Score: "+computerscore;
     }else{
-        alert("You play Scissors! I play Paper! You win!");
+        gameresult.innerHTML="You play Scissors! I play Paper! You win!";
         playerscore++;
         var scoreupdate = document.getElementById("player score");
         scoreupdate.innerHTML = "Your Score: "+playerscore;

c2e330bc4bd8878d1e507f653b190483a03327fd
Your Name <you@example.com>
2020-06-01 00:34:56 -0700

    nothing

--- IndexpageJS/index_aboutme.js
+++ IndexpageJS/index_aboutme.js
@@ -1,1 +1,5 @@
-var myfavoritenumber = 0;
+function aboutme(){
+    var myinfo=document.getElementById("displayhere");
+    myinfo.innerHTML="Hello everyone, I'm a fifth year graduate student in the Department of Mathematics at the University of California, Irvine.<br />";
+    myinfo.innerHTML+="<a href="http://www.math.uci.edu/~chenlong/"> Long Chen </a>.";
+}

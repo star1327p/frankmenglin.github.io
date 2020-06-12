c9ec4e86f63a6c87d47d6e41c0a6b0ba6c15462b
Your Name <you@example.com>
2020-06-12 16:57:03 -0700

    teaching summary

---
+++ JSpractice/js02_TeachingSummary.js
@@ -0,0 +1,8 @@
+function teachingsummary() {
+    alert("Frank: Alright!");
+    alert("Frank: I worked as teaching assistant (TA) at UCI from 2015 Fall to 2020.");
+    alert("Frank: I have led discussion sections and review sections in many classes.");
+    alert("Frank: For example, Calculus (2A,2B,2D,2E), Linear Algebra(3A), Matlab(9), Abstract Math(13)");
+    alert("Frank: Abstract Algebra(120A), Probability (130C), Real Analysis(140B), Math Finance (133A,133B,133C,176)");
+    alert("Frank: You can find some of my teaching videos at my home page -> Teaching Zoom Videos")
+}

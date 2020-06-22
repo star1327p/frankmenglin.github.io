939d5601eacb34509e37e22e3405dfa39c4e901d
Your Name <you@example.com>
2020-06-22 06:42:17 -0700

    solution of drawing game

--- JSpractice/js09_Drawing_Game.js
+++ JSpractice/js09_Drawing_Game.js
@@ -41,6 +41,51 @@ function hide_example(){
     buttontext.setAttribute("onClick","display_sample();");
 }
 
+function display_solution() {
+    sol = document.getElementById("gamesolution");
+    sol.innerHTML =  "This game can be solved recursively. We define <br>"
+    sol.innerHTML += "\\(n =\\) number of chances you and the opponent combine after this turn (if neither of you accept a number yet). We call this turn \\(n\\).<br>"
+    sol.innerHTML += "For example, if it is your turn to draw, after this draw, you have 2 more chances, your opponent has 3 more chances, then \\(n = 5\\) and we say you are at turn 5.<br>"
+    sol.innerHTML += "Notice it is different from what \\(n^{th} \\) turn means in convention, by our definition \\(n\\) <b>decrease</b> by 1 after each draw.<br>"
+    sol.innerHTML += "Now we define two function \\(A(n)\\) and \\(P(n)\\)<br>"
+    sol.innerHTML += "\\(A(n) = \\) The smallest number you should accept at turn \\(n\\)<br>"
+    sol.innerHTML += "\\(P(n) = \\) The probability you win the game, right before you draw a number at turn \\(n\\)<br>"
+    sol.innerHTML += "Consider the base case. It's your last chance to draw, your opponent has not yet accepted a number, and will draw (also last chance) after you.<br>"
+    sol.innerHTML += "By our definition, for the \\(n = 1\\) base case,<br>"
+    sol.innerHTML += "\\(A(1) = 0\\), because it is your last chance, you must accept.<br>"
+    sol.innerHTML += "\\(P(1) = 0.5\\), both you and your opponent are forced to accept the last result, so you and your opponent has identical chance to win.<br>"
+    sol.innerHTML += "Now suppose you know \\(A(n)\\) and \\(P(n)\\), and want to compute \\(A(n+1)\\) and \\(P(n+1)\\).<br>"
+    sol.innerHTML += "At turn \\(n+1\\) if you don't accept the number you draw, your opponent's chance to win is \\(P(n)\\). Therefore your chance to win if you decline is \\(1-P(n)\\).<br>"
+    sol.innerHTML += "On the other hand if you accept the number you draw (let's call it \\(x\\)), your opponent is left with \\(\\lfloor \\frac{n+1}{2} \\rfloor\\) chances to beat \\(x\\).<br>"
+    sol.innerHTML += "You need your opponent to draw less than \\(x\\) in all the remaining draws to win. Therefore your chance of winning if you accept \\(x\\) is \\(x^{\\lfloor \\frac{n+1}{2} \\rfloor}\\)<br>"
+    sol.innerHTML += "That means you should accept the number \\(x\\) if it satisfies \\( x^{\\lfloor \\frac{n+1}{2} \\rfloor} \\geq 1-P(n) \\).<br>"
+    sol.innerHTML += "In other words, $$ A(n+1) = (1-P(n))^{ \\frac{1}{\\lfloor \\frac{n+1}{2} \\rfloor} } $$"
+    sol.innerHTML += "To compute \\( P(n+1) \\), notice the probability you win before you draw this turn is (prob. of accept)*(prob. of winning if you accept)+(prob. of decline)*(prob. of winning if you decline) <br>"
+    sol.innerHTML += "For now we know probability of accept is \\( 1-A(n+1) \\) and probability of decline is \\( A(n+1) \\), probability of winning if decline is \\(1-P(n)\\) <br>"
+    sol.innerHTML += "It is left to solve probability of winning if you accept. The number you accept, \\(x \\) , has a uniform distribution on the range \\( [A(n+1),1] \\), so the probability density function is \\(f(x)=\\frac{1}{1-A(n+1)}\\) <br>"
+    sol.innerHTML += "That makes the probability of winning if you accept \\(= \\int_{A(n+1)}^{1} x^{\\lfloor \\frac{n+1}{2} \\rfloor}f(x) dx = \\frac{1-A(n+1)^{\\lfloor \\frac{n+1}{2} \\rfloor+1}}{( \\lfloor \\frac{n+1}{2} \\rfloor +1)(1-A(n+1))} \\) <br>"
+    sol.innerHTML += "Put everything together, $$ P(n+1) = (1-A(n+1)) \\times \\left( \\frac{1-A(n+1)^{\\lfloor \\frac{n+1}{2} \\rfloor+1}}{( \\lfloor \\frac{n+1}{2} \\rfloor +1)(1-A(n+1))}\\right)+A(n+1)\\times (1-P(n)) $$"
+    sol.innerHTML += "<hr />"
+    sol.innerHTML += "Finally the numerical solution of this game (when neither player has not accepted a number) is the following.<br>"
+    sol.innerHTML += "The minimum number the player that goes first should accept is 0.8538020735764572, 0.8153252042310043, 0.7496600557855093, 0.6123724356957945, 0<br>"
+    sol.innerHTML += "on the first to fifth draw (\\( n=9,7,5,3,1 \\)) correspondingly.<br>"
+    sol.innerHTML += "The minimum number the player that goes last should accept is 0.8458005857446074, 0.8005856451119897, 0.7166404661632764, 0.5, 0<br>"
+    sol.innerHTML += "on the first to fifth draw (\\( n=8,6,4,2,0 \\)) correspondingly.<br>"
+    sol.innerHTML += "This game favors the player that goes last, who has a winning probability 0.5105121796561449."
+    MathJax.Hub.Queue(["Typeset", MathJax.Hub]);
+    var buttontext = document.getElementById("solution");
+    buttontext.innerHTML = "Hide solution";
+    buttontext.setAttribute("onClick", "hide_solution();");
+}
+
+function hide_solution() {
+    sol = document.getElementById("gamesolution");
+    sol.innerHTML = "";
+    var buttontext = document.getElementById("solution");
+    buttontext.innerHTML = "Display the solution (best strategy) of this game";
+    buttontext.setAttribute("onClick", "display_solution();");
+}
+
 function cleargameinfo() {
     var game = document.getElementById("gameinfo");
     game.innerHTML = "";
